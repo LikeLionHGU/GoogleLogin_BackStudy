9c422051a46ce907d6feeb1e9f4ebd727f83585b
ìºzionkim <22100130@handong.ac.kr>
2025-03-05 10:21:15 +0900

    initial

---
+++ src/main/java/org/example/googlelogin_backstudy/GoogleLoginBackStudyApplication.java
@@ -0,0 +1,13 @@
+package org.example.googlelogin_backstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoogleLoginBackStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GoogleLoginBackStudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/googlelogin_backstudy/GoogleLoginBackStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.googlelogin_backstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoogleLoginBackStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
